5f79fc61759fc792672aecaeac14ac225912addd
Steven Nagle <stevenagle@outlook.com>
2021-12-18 21:02:37 -0500

    Framing SPA

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,16 @@
+import Header from './page_components/Header'
+import Footer from './page_components/Footer'
 
 function App() {
   return (
     <div >
- 
+      <Header />
+      <main>
+        <h1>
+          E-Store App
+        </h1>
+      </main>
+      <Footer />
     </div>
   );
 }
